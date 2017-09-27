0817eef44bb0301ad4f1dfdf1bc2c11cf8500ec9
Gary Bacon <pixelbud@gmail.com>
2017-09-26 21:58:17 -0400

    Shortcode for u-highlight

--- shortcodes.php
+++ shortcodes.php
@@ -1,21 +1,11 @@
 <?php
-  /* Shortcodes for the Tactics Dashboard
-   * Dependant on the WPcomplete plugin, which has to be activated.
+  /* Shortcodes
    */
-
-   // [course-contain] shortcode
-   function shortcode_course_contain( $atts, $content = null ) {
-
-     // create course container
-     return
-        '<div class="course-contain">' . do_shortcode($content) . '</div>';
-   }
-   add_shortcode('course-contain', 'shortcode_course_contain');
 
    // [mm-header] shortcode
    function shortcode_mm_header( $atts, $content = null ) {
 
-     // create course
+     // create expanded header for page template
      return
 
        '<div class="break"><header class="header header--wp header--centered header--mm"><div class="container container--widened">' . do_shortcode($content) . '</div></header></div>';
@@ -36,4 +26,19 @@
        '<a class="button ' . $atts[style] . '" href="' . $atts[link] . '" style="width:' . $atts[width] . ';">' . do_shortcode($content) . '</a>';
    }
    add_shortcode('mm-button', 'shortcode_mm_button');
+
+   // [u-highlight] shortcode
+   function shortcode_mm_uhighlight( $atts, $content = null ) {
+     $atts = shortcode_atts(
+        array(
+            'style' => '',
+            'link' => '#',
+            'width' => '100%'
+        ), $atts, 'mm-button'
+      );
+     // create course
+     return
+      '<div class="container container--normal u-highlight">' . do_shortcode($content) . '</div>';
+   }
+   add_shortcode('u-highlight', 'shortcode_mm_uhighlight');
  ?>
